058486293d900aa8a772ed7191b44587a684bb47
akavalera <a.kavalersky@live.com>
2018-10-04 14:00:06 +0300

    Update ParseCallback.java

--- src/main/java/org/parse4j/callback/ParseCallback.java
+++ src/main/java/org/parse4j/callback/ParseCallback.java
@@ -2,8 +2,8 @@ package org.parse4j.callback;
 
 import org.parse4j.ParseException;
 
-public abstract class ParseCallback<T> {
+public interfcae ParseCallback<T> {
 	
-	abstract void internalDone(T paramT, ParseException parseException);
+	public void internalDone(T paramT, ParseException parseException);
 	
 }
