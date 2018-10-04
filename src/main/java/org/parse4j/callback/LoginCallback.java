03ea21b8c7faa05b5b67102688c1f3f42ff110cf
akavalera <a.kavalersky@live.com>
2018-10-04 13:35:57 +0300

    Update LoginCallback.java
    Added body to non abstract method

--- src/main/java/org/parse4j/callback/LoginCallback.java
+++ src/main/java/org/parse4j/callback/LoginCallback.java
@@ -5,7 +5,7 @@ import org.parse4j.ParseUser;
 
 public class LoginCallback extends ParseCallback<ParseUser> {
 
-	void done(ParseUser parseUser, ParseException parseException);
+	void done(ParseUser parseUser, ParseException parseException) {}
 	
 	@Override
 	void internalDone(ParseUser parseUser, ParseException parseException) {
