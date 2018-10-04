ac6902dc747124ed6c3252f33906026a3ea55545
akavalera <a.kavalersky@live.com>
2018-10-04 13:24:51 +0300

    Update LoginCallback.java
    Removed unnecessary abstract class attribute

--- src/main/java/org/parse4j/callback/LoginCallback.java
+++ src/main/java/org/parse4j/callback/LoginCallback.java
@@ -3,9 +3,9 @@ package org.parse4j.callback;
 import org.parse4j.ParseException;
 import org.parse4j.ParseUser;
 
-public abstract class LoginCallback extends ParseCallback<ParseUser> {
+public class LoginCallback extends ParseCallback<ParseUser> {
 
-	abstract void done(ParseUser parseUser, ParseException parseException);
+	void done(ParseUser parseUser, ParseException parseException);
 	
 	@Override
 	void internalDone(ParseUser parseUser, ParseException parseException) {
